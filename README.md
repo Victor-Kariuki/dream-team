51c5f704f45381b2665801f250d3d7ebf5d58f45
Victor Kariuki <karizvic@gmail.com>
2020-09-12 13:34:39 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Dream Team
+
+> Employee management web app.
+
+## Installation
+
+```bash
+### Clone the git repository
+git clone
+
+### Install project dependencies
+npm install
+
+```
+
+### Author
+
+- [Victor Kariuki](mailto:karizvic@gmail.com)
---
+++ config/database.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_CONNECTION
+  },
+  test: {
+    username: process.env.TEST_DB_USERNAME,
+    password: process.env.TEST_DB_PASSWORD,
+    database: process.env.TEST_DB_DATABASE,
+    host: process.env.TEST_DB_HOST,
+    dialect: process.env.TEST_DB_CONNECTION
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_DATABASE,
+    host: process.env.PROD_DB_HOST,
+    dialect: process.env.PROD_DB_CONNECTION
+  }
+}
